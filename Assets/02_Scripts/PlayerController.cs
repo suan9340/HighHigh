b04ba36d7b883ee8fc63893b1a547fd24f9269fe
suan <sue9340@gmail.com>
2023-06-22 22:56:22 +0900

    라인 렌더러 + 해당 위치로 이동하기 + 눌렀을 때만 레이나오게 하는거~

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -2,35 +2,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class PlayerController : MonoBehaviour
 {
     private RaycastHit hitInfo;
     private Ray ray;
 
-    private Camera mainCam = null;
 
     [Header("RayCastStartPosition")]
     public Transform rayStartTrn = null;
     public float maxRay = 100f;
 
+
+    private Camera mainCam = null;
+    private LineRenderer myLineRen = null;
+
+    private bool isShootLine = false;
     private void Start()
     {
         mainCam = Camera.main;
+        myLineRen = GetComponent<LineRenderer>();
+
+        DontShootLineRenderer();
     }
 
     private void Update()
     {
-        Debug.DrawRay(rayStartTrn.position, mainCam.transform.forward * 100, Color.red);
-        CheckHit();
+        InputKey();
+    }
+
+    private void InputKey()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            CheckHit();
+            ShootLineRenderer();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isShootLine = false;
+            DontShootLineRenderer();
+        }
+    }
+
+    private void ShootLineRenderer()
+    {
+        myLineRen.positionCount = 2;
+        myLineRen.SetPosition(0, rayStartTrn.position);
+        myLineRen.SetPosition(1, mainCam.transform.forward * 100);
+    }
+
+    private void DontShootLineRenderer()
+    {
+        myLineRen.positionCount = 0;
     }
 
     private void CheckHit()
     {
         if (Physics.Raycast(rayStartTrn.position, mainCam.transform.forward, out hitInfo, maxRay))
         {
-            Debug.Log(hitInfo.transform.name);
+            if (hitInfo.collider.CompareTag("Wall"))
+            {
+                if (isShootLine)
+                {
+                    return;
+                }
+
+                isShootLine = true;
+                MovePlayer(hitInfo.point);
+            }
         }
 
+    }
+
+    private void MovePlayer(Vector3 _endPos)
+    {
+        Debug.Log(_endPos);
+
+        transform.position = _endPos;
     }
 
 
